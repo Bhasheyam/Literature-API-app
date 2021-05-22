9b10ca53d144dfb6db4acb3486e926bc515c871e
anandraj-coder <84544133+anandraj-coder@users.noreply.github.com>
2021-05-22 11:51:14 -0700

    First commit from Anand

--- app/app.go
+++ app/app.go
@@ -1,15 +1,16 @@
 package main
 
 import (
-    "fmt"
-    "net/http"
+	"fmt"
+	"net/http"
 )
 
 func main() {
-    http.HandleFunc("/", HelloServer)
-    http.ListenAndServe(":9000", nil)
+	http.HandleFunc("/", HelloServer)
+	http.ListenAndServe(":9000", nil)
 }
 
 func HelloServer(w http.ResponseWriter, r *http.Request) {
-    fmt.Fprintf(w, "Hello go, %s!", r.URL.Path[1:])
+	fmt.Fprintf(w, "Hello go from Let's Learn, %s!", r.URL.Path[1:])
+	fmt.Fprintf(w, "Hello world from Anand")
 }
