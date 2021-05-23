ed2ffa0bddb6e60950bcdb645ba811cb785522bb
anandraj-coder <84544133+anandraj-coder@users.noreply.github.com>
2021-05-23 16:39:58 -0700

    Added URL

--- app/app.go
+++ app/app.go
@@ -6,12 +6,12 @@ import (
 )
 
 func main() {
-	http.HandleFunc("/", HelloServer)
+	http.HandleFunc("/HelloWorld", HelloServer)
 	http.ListenAndServe(":9000", nil)
 }
 
 func HelloServer(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Hello go from Let's Learn, %s!\n", r.URL.Path[1:])
+	fmt.Fprintf(w, "Hello go from Let's Learn, %s!\n", r.URL)
 	fmt.Fprintf(w, "Hello world from Anand\n")
 	fmt.Fprintf(w, "Hi Everyone, guess who\n")
 }
